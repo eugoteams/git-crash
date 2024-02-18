58883016c3251cc1555c5bf79cc8fa51658cc569
maturi karthik <maturikarthik@maturis-MacBook-Pro.local>
2024-02-18 22:13:25 +0530

    feat/IIFE

---
+++ index.js
@@ -0,0 +1,19 @@
+/** @format */
+
+let privateValue = (() => {
+  let value = 0;
+
+  function setValue(userValue) {
+    value = userValue;
+  }
+
+  function getValue() {
+    return value;
+  }
+  return { setValue, getValue };
+})();
+
+privateValue.setValue(52);
+console.log(privateValue.getValue());
+privateValue.setValue(5);
+console.log(privateValue.getValue());
